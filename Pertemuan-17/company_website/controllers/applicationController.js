cba3818c7c0f66726ccc6d4e642e32030b4ec6c6
Fajrul Aslim <fajrul.aslim@gmail.com>
2019-11-19 21:41:07 +0700

    Pertemuan-17

--- Pertemuan-17/company_website/controllers/applicationController.js
+++ Pertemuan-17/company_website/controllers/applicationController.js
@@ -1,24 +1,24 @@
 let Cat = require('../models/cat.js');
 
-exports.home = function(req, res, next){
-    res.render('index', {
-        title: 'Belajar Express',
-        author: 'Fajrul'
-    })
+exports.home = function (req, res, next) {
+  res.render('index', {
+    title: 'Learning Express JS',
+    author: 'Fajrul'
+  })
 }
 
-exports.about = function(req, res, next){
-    res.render('home/about');
+exports.about = function (req, res, next) {
+  res.render('home/about', { title: 'About' });
 }
 
-exports.contact = function(req, res, next){
-    res.render('home/contact');
+exports.contact = function (req, res, next) {
+  res.render('home/contact', { title: 'Contact' });
 }
 
-exports.cats = function(req, res, next) {
-  Cat.find({}, function(err, results) {
+exports.cats = function (req, res, next) {
+  Cat.find({}, function (err, results) {
     res.render('cats', {
-      data: results 
+      data: results
     });
   });
 }
